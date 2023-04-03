30dedfaaf926c0e051c473e84608898e95000c30
Jake <jdwhitmore3@gmail.com>
2023-04-03 14:03:51 -0500

    Added options for user

--- Program.cs
+++ Program.cs
@@ -8,27 +8,42 @@ string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
 
+Console.WriteLine("1. Display all blogs");
+Console.WriteLine("2. Add blog");
+Console.WriteLine("3. Create post");
+Console.WriteLine("4. Display posts");
+
+string input = Console.ReadLine();
+
 try
 {
-
-    // Create and save a new Blog
-    Console.Write("Enter a name for a new Blog: ");
-    var name = Console.ReadLine();
-
-    var blog = new Blog { Name = name };
-
-    var db = new BloggingContext();
-    db.AddBlog(blog);
-    logger.Info("Blog added - {name}", name);
-
-    // Display all Blogs from the database
-    var query = db.Blogs.OrderBy(b => b.Name);
-
-    Console.WriteLine("All blogs in the database:");
-    foreach (var item in query)
+    if (input == "2")
     {
-        Console.WriteLine(item.Name);
+        // Create and save a new Blog
+        Console.Write("Enter a name for a new Blog: ");
+        var name = Console.ReadLine();
+
+        var blog = new Blog { Name = name };
+
+        var db = new BloggingContext();
+        db.AddBlog(blog);
+        logger.Info("Blog added - {name}", name);
     }
+
+    if (input == "1")
+    {
+        // Display all Blogs from the database
+        var db = new BloggingContext();
+
+        var query = db.Blogs.OrderBy(b => b.Name);
+
+        Console.WriteLine("All blogs in the database:");
+        foreach (var item in query)
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+
 }
 catch (Exception ex)
 {
