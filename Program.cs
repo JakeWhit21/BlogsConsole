4ac36334608444a54c3f2cbc423d82ee14290a94
Jake <jdwhitmore3@gmail.com>
2023-04-03 15:34:12 -0500

    Completed create post functionality

--- Program.cs
+++ Program.cs
@@ -15,25 +15,14 @@ Console.WriteLine("4. Display posts");
 
 string input = Console.ReadLine();
 
+var db = new BloggingContext();
+
 try
 {
-    if (input == "2")
-    {
-        // Create and save a new Blog
-        Console.Write("Enter a name for a new Blog: ");
-        var name = Console.ReadLine();
-
-        var blog = new Blog { Name = name };
-
-        var db = new BloggingContext();
-        db.AddBlog(blog);
-        logger.Info("Blog added - {name}", name);
-    }
 
     if (input == "1")
     {
         // Display all Blogs from the database
-        var db = new BloggingContext();
 
         var query = db.Blogs.OrderBy(b => b.Name);
 
@@ -42,6 +31,71 @@ try
         {
             Console.WriteLine(item.Name);
         }
+    }
+
+    if (input == "2")
+    {
+        // Create and save a new Blog
+        Console.Write("Enter a name for a new Blog: ");
+        var name = Console.ReadLine();
+
+        var blog = new Blog { Name = name };
+
+        db.AddBlog(blog);
+        logger.Info("Blog added - {name}", name);
+    }
+
+    if (input == "3")
+    {
+        //Create post in selected blog
+        Console.WriteLine("Select blog to post to: ");
+        string selectedBlog = Console.ReadLine();
+
+        Post post = new Post();
+        post.BlogId = db.Blogs.Where(b => b.Name.Equals(selectedBlog)).Select(b => b.BlogId).First();
+
+        Console.WriteLine("Enter title for post");
+        post.Title = Console.ReadLine();
+
+        Console.WriteLine("Enter content for post");
+        post.Content = Console.ReadLine();
+
+        db.AddPost(post);
+        logger.Info("Post added - {title}", post.Title);
+
+
+
+        //use post.(paramater) = ...
+
+        //use db.AddPost(post);
+
+
+        // foreach (var blog in db.Blogs)
+        // {
+        //     if (blog.BlogId == foundBlogId)
+        //     {
+        //         Console.WriteLine("Enter title for post: ");
+        //         string title = Console.ReadLine();
+
+        //         Console.WriteLine("Enter content for the post: ");
+        //         string content = Console.ReadLine();
+
+        //         Post post = new Post
+        //         {
+        //             Title = title,
+        //             Content = content,
+        //             BlogId = foundBlogId,
+        //             Blog = blog
+        //         };
+
+        //         blog.Posts.Add(post);
+        //         logger.Info("Post added - {title}", title);
+        //     }
+        // }
+
+
+
+
     }
 
 }
